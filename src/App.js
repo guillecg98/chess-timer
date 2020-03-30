384213280171c55ddea0f721166258d0003e230b
Guille Cosano <i62cogag@uco.es>
2020-03-30 20:45:28 +0200

    Created a timer for playing real chess

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ChangeTurnButton from './components/ChangeTurnButton'
+import StartGameButton from './components/StartGameButton'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        Chess Timer
+      </div>
+      <div>
+        <h3>Select Game Time per Player</h3>
+        <input type="number" required/>
+      </div>
+      <StartGameButton text="Start Game"/>
+        <div className="columnStyle">
+          <p>White Player</p>
+          <p>Black Player</p>
+          <img src={require('./img/white-pawn.png')} alt="white pawn" height="150" width="150"/>
+          <img src={require('./img/black-pawn.png')} alt="black pawn" height="150" width="150"/>
+        </div>
+        <div className="columnStyle">
+          <p>Counter 1</p>
+          <p>Counter 2</p>
+        </div>
+        <ChangeTurnButton text="'s Turn"/>
     </div>
   );
 }
